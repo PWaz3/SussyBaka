ffdf4746eb239bcddfa18b114d8c79822f50f563
PWaz3 <153152711+PWaz3@users.noreply.github.com>
2023-12-06 21:24:21 -0600

    Update oneclicke.blade.php

--- oneclicke.blade.php
+++ oneclicke.blade.php
@@ -1,1 +1,18 @@
-o
+@extends('device.index')
+@section('tab')
+
+<html>
+<head>
+    <title>Clickable Button</title>
+</head>
+<body>
+
+<form method="post" action="{{ route('executePythonScript') }}">
+    @csrf
+    <button type="submit" name="myButton" style = "border: 2px solid #000; padding: 5px;">Click for Oneclick Diagnostics</button>
+</form>
+
+</body>
+</html>
+
+@endsection
